cf8f4a5a1e8be5fbb8ea4c9dac9f49b183d5a426
watabee <watabee.dev@gmail.com>
2021-02-22 16:08:39 +0900

    Lifecycle 2.3.0

--- buildSrc/src/main/java/dependencies/dependencies.kt
+++ buildSrc/src/main/java/dependencies/dependencies.kt
@@ -54,9 +54,9 @@ object Deps {
         }
 
         object Lifecycle {
-            const val livedata = "androidx.lifecycle:lifecycle-livedata-ktx:2.3.0-beta01"
-            const val compiler = "androidx.lifecycle:lifecycle-compiler:2.3.0-beta01"
-            const val viewmodel = "androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.0-beta01"
+            const val livedata = "androidx.lifecycle:lifecycle-livedata-ktx:2.3.0"
+            const val compiler = "androidx.lifecycle:lifecycle-compiler:2.3.0"
+            const val viewmodel = "androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.0"
         }
 
         object Compose {
