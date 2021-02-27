dbe5acfafb6cbd0779f1fdbe610949cee3b5741b
watabee <watabee.dev@gmail.com>
2021-02-27 15:19:41 +0900

    Tweak item list design

--- ui/common/src/main/java/com/github/watabee/qiitacompose/ui/theme/Theme.kt
+++ ui/common/src/main/java/com/github/watabee/qiitacompose/ui/theme/Theme.kt
@@ -1,34 +1,34 @@
 package com.github.watabee.qiitacompose.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.Colors
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import com.github.watabee.qiitacompose.ui.common.R
 
 // TODO: Set dark color palette
 private val DarkColorPalette = darkColors()
 
-private val LightColorPalette = lightColors(
-    primary = green500,
-    primaryVariant = green800,
-    secondary = blueGrey600,
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onBackground = grey800,
-    onSurface = grey800
-)
+@Composable
+private fun lightColorPalette(): Colors {
+    return lightColors(
+        primary = colorResource(id = R.color.green_500),
+        primaryVariant = colorResource(id = R.color.green_800),
+        secondary = colorResource(id = R.color.blue_grey_600),
+        background = colorResource(id = R.color.white),
+        surface = colorResource(id = R.color.white),
+        onPrimary = colorResource(id = R.color.white),
+        onBackground = colorResource(id = R.color.grey_900),
+        onSurface = colorResource(id = R.color.grey_900)
+    )
+}
 
 @Composable
 fun QiitaTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors =
-        if (darkTheme) {
-            DarkColorPalette
-        } else {
-            LightColorPalette
-        }
+    val colors = if (darkTheme) DarkColorPalette else lightColorPalette()
 
     MaterialTheme(colors = colors, typography = typography, shapes = shapes, content = content)
 }
--- ui/items/src/main/java/com/github/watabee/qiitacompose/ui/items/ItemListItem.kt
+++ ui/items/src/main/java/com/github/watabee/qiitacompose/ui/items/ItemListItem.kt
@@ -8,15 +8,19 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.requiredWidth
+import androidx.compose.material.ContentAlpha
 import androidx.compose.material.Divider
 import androidx.compose.material.Icon
+import androidx.compose.material.LocalContentAlpha
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ThumbUp
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.github.watabee.qiitacompose.api.response.Item
@@ -30,27 +34,36 @@ internal fun ItemListItem(item: Item) {
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(horizontal = 8.dp, vertical = 8.dp)
+                .padding(horizontal = 8.dp, vertical = 16.dp)
         ) {
-            Text(
-                text = "@${item.user.id} が${AppDateFormatter.dateToString("yyyy年MM月dd日", item.createdAt)}に投稿",
-                style = MaterialTheme.typography.caption
-            )
-            Spacer(modifier = Modifier.requiredHeight(4.dp))
-            Text(text = item.title, style = MaterialTheme.typography.h6)
-            Spacer(modifier = Modifier.requiredHeight(4.dp))
-            Row {
-//                Icon(imageVector = Icons.Filled.ThumbUp, contentDescription = null)
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
                 Text(
-                    text = item.tags.joinToString(", ") { it.name },
+                    text = "@${item.user.id} が${AppDateFormatter.dateToString("yyyy年MM月dd日", item.createdAt)}に投稿",
                     style = MaterialTheme.typography.caption
                 )
-            }
-            Spacer(modifier = Modifier.requiredHeight(4.dp))
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Icon(imageVector = Icons.Filled.ThumbUp, contentDescription = null, modifier = Modifier.requiredSize(16.dp))
-                Spacer(modifier = Modifier.requiredWidth(4.dp))
-                Text(text = item.likesCount.toString(), style = MaterialTheme.typography.caption)
+                Spacer(modifier = Modifier.requiredHeight(4.dp))
+                CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.high) {
+                    Text(text = item.title, style = MaterialTheme.typography.h6)
+                }
+                Spacer(modifier = Modifier.requiredHeight(8.dp))
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_items_tag),
+                        contentDescription = null,
+                        modifier = Modifier.requiredSize(16.dp)
+                    )
+                    Spacer(modifier = Modifier.requiredWidth(8.dp))
+                    Text(
+                        text = item.tags.joinToString(", ") { it.name },
+                        style = MaterialTheme.typography.caption
+                    )
+                }
+                Spacer(modifier = Modifier.requiredHeight(8.dp))
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(imageVector = Icons.Filled.ThumbUp, contentDescription = null, modifier = Modifier.requiredSize(16.dp))
+                    Spacer(modifier = Modifier.requiredWidth(8.dp))
+                    Text(text = item.likesCount.toString(), style = MaterialTheme.typography.caption)
+                }
             }
         }
         Divider()
@@ -81,7 +94,7 @@ private fun PreviewItemListItem() {
             reactionsCount = 20,
             pageViewsCount = 10,
             commentsCount = 5,
-            tags = listOf(Item.Tag("HTML"), Item.Tag("CSS"), Item.Tag("JavaScript"))
+            tags = listOf(Item.Tag("HTML"), Item.Tag("CSS"), Item.Tag("JavaScript"), Item.Tag("JavaScript"), Item.Tag("JavaScript"), Item.Tag("JavaScript"), Item.Tag("JavaScript"))
         )
     )
 }
