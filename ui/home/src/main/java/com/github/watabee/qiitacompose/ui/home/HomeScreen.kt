f430b1df7a07e987e7abc4716a47b3ee9f855795
watabee <watabee.dev@gmail.com>
2021-02-27 13:52:46 +0900

    Create home module

---
+++ ui/home/src/main/java/com/github/watabee/qiitacompose/ui/home/HomeScreen.kt
@@ -0,0 +1,136 @@
+package com.github.watabee.qiitacompose.ui.home
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.layout.requiredWidth
+import androidx.compose.material.Divider
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.ScaffoldState
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+import com.github.watabee.qiitacompose.ui.items.ItemsScreen
+import kotlinx.coroutines.launch
+
+@Composable
+fun HomeScreen(scaffoldState: ScaffoldState = rememberScaffoldState()) {
+    val coroutineScope = rememberCoroutineScope()
+    Scaffold(
+        scaffoldState = scaffoldState,
+        drawerContent = {
+            AppDrawer(closeDrawer = { coroutineScope.launch { scaffoldState.drawerState.close() } })
+        },
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Qiita") },
+                navigationIcon = {
+                    IconButton(onClick = { coroutineScope.launch { scaffoldState.drawerState.open() } }) {
+                        Icon(imageVector = Icons.Filled.Menu, contentDescription = null)
+                    }
+                }
+            )
+        },
+        content = {
+            ItemsScreen()
+        }
+    )
+}
+
+@Composable
+private fun AppDrawer(closeDrawer: () -> Unit) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Spacer(Modifier.requiredHeight(24.dp))
+        Divider(color = MaterialTheme.colors.onSurface.copy(alpha = .2f))
+        DrawerButton(
+            icon = Icons.Filled.Home,
+            label = "Home",
+            isSelected = true,
+            action = {
+                closeDrawer()
+            }
+        )
+    }
+}
+
+@Composable
+private fun DrawerButton(
+    icon: ImageVector,
+    label: String,
+    isSelected: Boolean,
+    action: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val colors = MaterialTheme.colors
+    val imageAlpha = if (isSelected) {
+        1f
+    } else {
+        0.6f
+    }
+    val textIconColor = if (isSelected) {
+        colors.primary
+    } else {
+        colors.onSurface.copy(alpha = 0.6f)
+    }
+    val backgroundColor = if (isSelected) {
+        colors.primary.copy(alpha = 0.12f)
+    } else {
+        Color.Transparent
+    }
+
+    val surfaceModifier = modifier
+        .padding(start = 8.dp, top = 8.dp, end = 8.dp)
+        .fillMaxWidth()
+
+    Surface(
+        modifier = surfaceModifier,
+        color = backgroundColor,
+        shape = MaterialTheme.shapes.small
+    ) {
+        TextButton(
+            onClick = action,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.Start,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Image(
+                    imageVector = icon,
+                    contentDescription = null, // decorative
+                    colorFilter = ColorFilter.tint(textIconColor),
+                    alpha = imageAlpha
+                )
+                Spacer(Modifier.requiredWidth(16.dp))
+                Text(
+                    text = label,
+                    style = MaterialTheme.typography.body2,
+                    color = textIconColor
+                )
+            }
+        }
+    }
+}
