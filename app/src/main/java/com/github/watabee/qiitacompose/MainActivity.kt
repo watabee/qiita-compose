e8ca9f9d5b32676ccfc421790dfadf1505b55db6
watabee <watabee.dev@gmail.com>
2022-02-26 18:59:22 +0900

    Remove fragment & navigation

--- app/src/main/java/com/github/watabee/qiitacompose/MainActivity.kt
+++ app/src/main/java/com/github/watabee/qiitacompose/MainActivity.kt
@@ -1,31 +1,42 @@
 package com.github.watabee.qiitacompose
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
 import androidx.core.view.WindowCompat
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
+import com.github.watabee.qiitacompose.repository.UserRepository
+import com.github.watabee.qiitacompose.ui.theme.QiitaTheme
+import com.github.watabee.qiitacompose.util.Env
+import com.google.accompanist.insets.ProvideWindowInsets
+import com.google.accompanist.insets.statusBarsPadding
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
+class MainActivity : AppCompatActivity() {
+
+    @Inject lateinit var userRepository: UserRepository
+    @Inject lateinit var env: Env
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         WindowCompat.setDecorFitsSystemWindows(window, false)
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        return findNavController().navigateUp() || super.onSupportNavigateUp()
-    }
-
-    /**
-     * See https://issuetracker.google.com/142847973
-     */
-    private fun findNavController(): NavController {
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        return navHostFragment.navController
+        setContent {
+            QiitaTheme {
+                ProvideWindowInsets {
+                    // A surface container using the 'background' color from the theme
+                    Surface(color = MaterialTheme.colors.background) {
+                        Column(modifier = Modifier.statusBarsPadding()) {
+                            NavGraph(env = env, userRepository = userRepository)
+                        }
+                    }
+                }
+            }
+        }
     }
 }
