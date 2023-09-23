d746237b348cd5386419f8b5028b0b939316c0be
watabee <watabee.dev@gmail.com>
2023-09-23 21:07:42 +0900

    Added instrumented test for SnsIconButtons

--- ui/common/src/main/java/com/github/watabee/qiitacompose/ui/common/SnsIconButtons.kt
+++ ui/common/src/main/java/com/github/watabee/qiitacompose/ui/common/SnsIconButtons.kt
@@ -5,34 +5,45 @@ import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.github.watabee.qiitacompose.ui.theme.QiitaTheme
 
 @Composable
-fun SnsIconButtons(githubLoginName: String?, twitterScreenName: String?, facebookId: String?, linkedinId: String?) {
+fun SnsIconButtons(
+    githubLoginName: String?,
+    twitterScreenName: String?,
+    facebookId: String?,
+    linkedinId: String?,
+    onGithubButtonClicked: () -> Unit = {},
+    onTwitterButtonClicked: () -> Unit = {},
+    onFacebookButtonClicked: () -> Unit = {},
+    onLinkedinButtonClicked: () -> Unit = {}
+) {
     if (githubLoginName.isNullOrBlank() && twitterScreenName.isNullOrBlank() && facebookId.isNullOrBlank() && linkedinId.isNullOrBlank()) {
         return
     }
     Row(verticalAlignment = Alignment.CenterVertically) {
         if (!githubLoginName.isNullOrBlank()) {
-            IconButton(onClick = { /*TODO*/ }) {
+            IconButton(modifier = Modifier.testTag("GithubButton"), onClick = onGithubButtonClicked) {
                 Icon(painter = painterResource(id = R.drawable.ic_user_github), contentDescription = null, tint = Color(0xFF333333))
             }
         }
         if (!twitterScreenName.isNullOrBlank()) {
-            IconButton(onClick = { /*TODO*/ }) {
+            IconButton(modifier = Modifier.testTag("TwitterButton"), onClick = onTwitterButtonClicked) {
                 Icon(painter = painterResource(id = R.drawable.ic_user_twitter), contentDescription = null, tint = Color(0xFF1DA1F2))
             }
         }
         if (!facebookId.isNullOrBlank()) {
-            IconButton(onClick = { /*TODO*/ }) {
+            IconButton(modifier = Modifier.testTag("FacebookButton"), onClick = onFacebookButtonClicked) {
                 Icon(painter = painterResource(id = R.drawable.ic_user_facebook), contentDescription = null, tint = Color(0xFF1778F2))
             }
         }
         if (!linkedinId.isNullOrBlank()) {
-            IconButton(onClick = { /*TODO*/ }) {
+            IconButton(modifier = Modifier.testTag("LinkedinButton"), onClick = onLinkedinButtonClicked) {
                 Icon(painter = painterResource(id = R.drawable.ic_user_linkedin), contentDescription = null, tint = Color(0xFF0077B5))
             }
         }
